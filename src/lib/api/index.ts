43caecac8b4a5224cc57ac3abbfee27a6bb3c0fb
RafiAbqor <rafi.abqor@gmail.com>
2024-09-02 12:27:26 +0700

    updateURLNew

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://pctrn-3u5lpwav5-rafi-abdurrahman-abqoris-projects.vercel.app/"
+    baseURL: "https://pctrn-be.vercel.app/"
 });
 
 export const setAuthToken = (token?: string) => {
