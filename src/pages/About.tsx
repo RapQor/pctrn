0dbc1069609d83781cea9da83e6432cb0fdf0454
RafiAbqor <rafi.abqor@gmail.com>
2024-09-02 12:04:10 +0700

    new-error

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,15 +1,15 @@
-import Tittle from "../components/about/Tittle"
+// import Tittle from "../components/about/Tittle"
 
 
-const About = () => {
-  return (
-    <>
-        <Tittle tittle="About"/>
-        <div>
-            About from pages
-        </div>
-    </>
-  );
-}
+// const About = () => {
+//   return (
+//     <>
+//         <Tittle tittle="About"/>
+//         <div>
+//             About from pages
+//         </div>
+//     </>
+//   );
+// }
 
-export default About
+// export default About
