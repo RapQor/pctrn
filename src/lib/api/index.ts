4ff9c2656e822f27c53e8a4ae884f4c5c5d77a68
RafiAbqor <rafi.abqor@gmail.com>
2024-09-02 12:46:23 +0700

    updateURLLagi

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://pctrn-be.vercel.app/"
+    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000"
 });
 
 export const setAuthToken = (token?: string) => {
