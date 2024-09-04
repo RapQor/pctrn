ad35a581811bc54ca9cd17069fde263a82a7579f
RafiAbqor <rafi.abqor@gmail.com>
2024-09-04 09:49:42 +0700

    update-detail

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -104,13 +104,11 @@ const DetailIndex = () => {
               <Box sx={{ marginTop: 2 }}>
                 {post.images.map((image, index) => (
                   <img
-                  key={index}
-                  src={`http://localhost:5000/uploads/${image.image}`}
-                  alt={`post image ${index + 1}`}
-                  style={{ maxWidth: "100%", borderRadius: "10px" }}
+                    key={index}
+                    src={image.image}
+                    alt={`post image ${index + 1}`}
+                    style={{ maxWidth: "100%", borderRadius: "10px" }}
                   />
-                  // <Typography>{image.image}</Typography>
-
                 ))} 
               </Box>
             )}
